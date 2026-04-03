8b3863c0221df07ae30b91688b6778859928326b
Tom edison <devubuntux@gmail.com>
2026-04-03 22:46:18 +0800

    check fixed.

--- consensus/parlia/parlia.go
+++ consensus/parlia/parlia.go
@@ -60,8 +60,8 @@ const (
 	maxwellEpochLength   uint64 = 1000 // Epoch length starting from the Maxwell hard fork
 	defaultBlockInterval uint64 = 3000 // Default block interval in milliseconds
 	lorentzBlockInterval uint64 = 1500 // Block interval starting from the Lorentz hard fork
-	maxwellBlockInterval uint64 = 500  // Block interval starting from the Maxwell hard fork
-	fermiBlockInterval   uint64 = 450  // Block interval starting from the Fermi hard fork
+	maxwellBlockInterval uint64 = 750  // Block interval starting from the Maxwell hard fork
+	fermiBlockInterval   uint64 = 500  // Block interval starting from the Fermi hard fork
 	defaultTurnLength    uint8  = 1    // Default consecutive number of blocks a validator receives priority for block production
 
 	extraVanity      = 32 // Fixed number of extra-data prefix bytes reserved for signer vanity
